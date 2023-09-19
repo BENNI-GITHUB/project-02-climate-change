899988a9c50f96270e4136f20dce1aae0c36628b
Behnam Pordavam <behnampordavam@gmail.com>
2023-09-19 11:46:25 +0000

    copying questions.js code from VScode to workspace

---
+++ assets/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        number: 1,
+        question: "All of the gases on the planet make up the earth's…",
+        answer: "b. Atmosphere",
+        options: [
+            "a. Biosphere",
+            "b. Atmosphere",
+            "c. Barometer",
+            "d. Climate"
+        ]
+    },
+    {
+        number: 2,
+        question: "The pattern of weather conditions over many years is called…",
+        answer: "b. Climate",
+        options: [
+            "a. Forecast",
+            "b. Climate",
+            "c. Temperature",
+            "d. Atlas"
+        ]
+    },
+    {
+        number: 3,
+        question: "The increase in the average temperature of the earth's atmosphere is referred to by scientists as…",
+        answer: "a. Global heating",
+        options:[
+            "a. Global heating",
+            "b. Thermo-climate",
+            "c. Earth charge",
+            "d. Core temperature change"
+        ]
+    },
+    {
+        number: 4,
+        question: "What is the name of gas released into the atmosphere when we burn fossil fuel for energy like coal, oil and natural gas?",
+        answer: "c. Carbon dioxide (CO2)",
+        options: [
+            "a. Plutonium",
+            "b. Oxygen",
+            "c. Carbon dioxide (CO2)",
+            "d. Hydrogen"
+        ]
+    },
+    {
+        number: 5,
+        question: "Which of the following sectors does not contribute to greenhouse gas emissions?",
+        answer: "d. They all do",
+        options: [
+            "a. Transportation",
+            "b. Deforestation",
+            "c. Agriculture",
+            "d. They all do"
+        ]
+    },
+    {
+        number: 6,
+        question: "Which could be a direct/indirect impact of climate change on children's health?",
+        answer: "d. All of the above",
+        options: [
+            "a. Lack of access to healthcare due to migration",
+            "b. Shortened food supply and poor nutrition",
+            "c. Limited access to clean water and increased water borne illness",
+            "d. All of the above"
+        ]
+    },
+    {
+        number: 7,
+        question: "What are the gases called that become trapped in the earth's atmosphere and are heating the planet?",
+        answer: "c. Greenhouse gases",
+        options: [
+            "a. Humidity",
+            "b. Precipitation",
+            "c. Greenhouse gases",
+            "d. Nitrates"
+        ]
+    },
+    {
+        number: 8,
+        question: "What did countries agree to in the famous “Paris Agreement” in 2015?",
+        answer: "a. To keep global temperature rise below 2°C and pursue a path to limit warming to 1.5°C",
+        options: [
+            "a. To keep global temperature rise below 2°C and pursue a path to limit warming to 1.5°C",
+            "b. To stop countries from dumping rubbish in the ocean",
+            "c. To transition to all electric cars by 2050",
+            "d. To plant 1 million trees in deforested areas"
+        ]
+    },
+    {
+        number: 9,
+        question: "Which one of the activities below is an action you can take to reduce your carbon footprint?",
+        answer: "d. All of the above",
+        options: [
+            "a. Eat less meat",
+            "b. Plant a tree",
+            "c. Wear second-hand fashion",
+            "d. All of the above"
+        ]
+    },
+    {
+        number: 10,
+        question: "Which is the most important day young people can take action on climate change?",
+        answer: "c. Every day",
+        options: [
+            "a. Earth Day",
+            "b. World Environment Day",
+            "c. Every day",
+            "d. World Cleanup Day"
+        ]
+    },
+];
